996c2232483b63272a58b446c9bc864b8223add1
ruddor <ruddor@live.com>
2020-07-02 11:17:13 -0400

    controller

---
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public string GetAllBooks()
+        {
+            return "All books";
+        }
+
+        public string GetBook(int id)
+        {
+            return $"book with id: {id}";
+        }
+
+        public string SearchBook(string bookName, string authorName)
+        {
+            return $"book with name: {bookName}, author: {authorName}";
+        }
+    }
+}
