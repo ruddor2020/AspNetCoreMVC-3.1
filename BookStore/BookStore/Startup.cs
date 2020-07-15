203bf872d2547ba6de0e47fb44b19bfd45657045
ruddor <ruddor@live.com>
2020-07-15 16:07:44 -0400

    static files

--- BookStore/BookStore/Startup.cs
+++ BookStore/BookStore/Startup.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace BookStore
@@ -29,6 +31,12 @@ namespace BookStore
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory() + "\\MyStaticFiles")),
+                RequestPath = "/MyStaticFiles"
+            });
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
