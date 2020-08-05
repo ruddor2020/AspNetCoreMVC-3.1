5a47d3cb9abe2c5b608064394664c4a0d3213a2a
ruddor <ruddor@live.com>
2020-08-05 14:53:42 -0400

    view data

--- BookStore/BookStore/Controllers/HomeController.cs
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Dynamic;
+using BookStore.Models;
 
 namespace BookStore.Controllers
 {
@@ -10,6 +12,14 @@ namespace BookStore.Controllers
     {
         public ViewResult Index()
         {
+            ViewBag.Title = "Ruddor";
+            dynamic data = new ExpandoObject();
+            data.Id = 1;
+            data.Name = "Ruddor";
+            ViewBag.Data = data;
+            ViewBag.Type = new BookModel() { Id = 5, Author = "John Doe" };
+            ViewData["Description"] = "This is description.";
+            ViewData["Book"] = new BookModel() { Id = 6, Author = "John Smith" };
             return View();
         }
 
